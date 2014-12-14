4620e4e04f81ca448e833de283cd800596ec4278
AdrianMLin <adrian.mx.lin@gmail.com>
2014-12-13 22:12:31 -0500

    column win check works now

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,20 @@
 var Quantme = Quantme || { Models: {}, Collections: {}, Views: {} };
 
+fullCategoryCollection = ""
+fullCategoryListView = ""
+
 Quantme.initialize = function(){
 	console.log('Quantme initialised!');
 
-	// -* COLLECTION *-
-	fullCategoryCollection = new Quantme.Collections.CategoryCollection({});
+	//-* MODEL *-
+	var category = new Quantme.Models.CategoryModel({});
 
+	// -* COLLECTION *-
+	fullCategoryCollection = new Quantme.Collections.CategoryCollection({
+		model: category
+	});
+
+	// -* LIST VIEW *-
 	fullCategoryListView = new Quantme.Views.CategoryListView({
 		collection: fullCategoryCollection
 	});
